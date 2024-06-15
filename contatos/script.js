9ba88eed86b764d6933897f5a91091d28c03d2cf
João Vitor <darosa.joao@yahoo.com.br>
2024-06-15 00:57:31 -0300

    Reescrevendo o JS e um pouco do HTML. Com auxílio do JetBrains WebStorm, StackOverflow e umas IAs haha

--- contatos/script.js
+++ contatos/script.js
@@ -1,40 +1,38 @@
 // esperar por esse tal de "dom" carregar
 // não sei o que muda na prática, mas foi o que fez o código funcionar
 // vi no stackoverflow e funcionou, ele cria uma função vazia e faz o que tem dentro
-document.addEventListener('DOMContentLoaded', function () { 
+document.addEventListener('DOMContentLoaded', () => {
+    const formulario = document.querySelector('#formulario'); // acha o formulário no html
+    const tabela = document.querySelector('#tabela'); // acha a tabela
+    formulario.addEventListener('submit', (event) => {
+        // não enviar o formulário pela url (com os parâmetros ? e &), em vez disso o js toma conta
+        event.preventDefault();
 
-    // esperar pelo usuário enviar o formulário
-    document.getElementById('formulario').addEventListener('submit', function (event) {
-        event.preventDefault() // não enviar o formulário pela url, em vez disso o js toma conta
+        // pegando as variáveis do formulário e jogando para essa constante
+        const {nome, celular, email, uf, cidade} = event.target.elements;
 
-        // pegando as variáveis direto do formulário
-        let nome = document.getElementById('nome').value;
-        let cel = document.getElementById('celular').value;
-        let email = document.getElementById('e-mail').value;
-        let estado = document.getElementById('uf').value;
-        let cidade = document.getElementById('cidade').value;
+        // passando os parâmetros para a função abaixo
+        adicionarContato(nome.value, celular.value, email.value, uf.value, cidade.value);
 
-        adicionarContato(nome, cel, email, estado, cidade); // função para adicionar o contato
-        document.getElementById('formulario').reset(); // resetando o formulário
+        formulario.reset(); // resetando o furmulário
     });
 
-    function adicionarContato(nome, cel, email, estado, cidade) {
-        const tabela = document.getElementById('tabela'); // acha a tabela e joga pra constante
+    // Função numa const para adicionar o contato
+    const adicionarContato = (nome, celular, email, uf, cidade) => {
+        const novaLinha = tabela.insertRow();
 
-        const novaLinha = tabela.insertRow(); // criar uma linha para jogar as informações
-
-        // consts para jogar as informações de cada variável em célula
+        // Insere uma nova célula para cada e joga o conteúdo...
         const celulaNome = novaLinha.insertCell(0);
-        const celulaCel = novaLinha.insertCell(1);
+        const celulaCelular = novaLinha.insertCell(1);
         const celulaEmail = novaLinha.insertCell(2);
-        const celulaEstado = novaLinha.insertCell(3);
+        const celulauf = novaLinha.insertCell(3);
         const celulaCidade = novaLinha.insertCell(4);
 
-        // passa a variável como string para a constante que adiciona a célula
-        celulaNome.textContent = nome;
-        celulaCel.textContent = cel;
-        celulaEmail.textContent = email;
-        celulaEstado.textContent = estado;
-        celulaCidade.textContent = cidade;
-    }
-})
+        // ...daqui
+        celulaNome.textContent = nome
+        celulaCelular.textContent = celular
+        celulaEmail.textContent = email
+        celulauf.textContent = uf
+        celulaCidade.textContent = cidade
+    };
+});
